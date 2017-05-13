05c7be25afece9533fb38295ef9f55b21cc9aeb2
David Cady Jr <davidcadyjr@mac.com>
2017-05-13 13:24:36 -0500

    unit 11 assignment

--- js/main.js
+++ js/main.js
@@ -34,19 +34,35 @@ var checkForMatch = function (){   //tells user if they got a match
 
 };
 
-var flipCard = function (cardId) { //tells user card they flipped
+var flipCard = function () { //tells user card they flipped
+	var cardId = this.getAttribute("data-id");
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
     console.log(cards[cardId].suit);
 
     cardsInPlay.push(cards[cardId].rank); //adds flipped card to array
     
+    this.setAttribute("src", cards[cardId].cardImage);
 
     if (cardsInPlay.length === 2) 
 
 	checkForMatch();
 
 }; 
+//unit 11  adds cards to board
+var createBoard = function (){
+	for (var i = 0; i < cards.length; i++) {
+	
+	var cardElement = document.createElement("img");
+	cardElement.setAttribute("src", "images/back.png");	
+	cardElement.setAttribute("data-id", i);
+	cardElement.addEventListener("click", flipCard);
+	document.getElementById("game-board").appendChild(cardElement)
 
-flipCard(0);
-flipCard(2);
+
+
+	}
+
+};
+
+createBoard ();
